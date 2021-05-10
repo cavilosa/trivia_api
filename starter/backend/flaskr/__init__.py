19f4d64562ec6b041b55baa7af183e7a7199ea5a
cavilosa <cavilosa33@gmail.com>
2021-05-10 15:53:29 -0600

    Deleted acces-control-origin from the headders

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -46,7 +46,6 @@ def create_app(test_config=None):
             'Access-Control-Allow-Headers',
             'Content-Type,Authorization,true')
         response.headers.add('Access-Control-Allow-Credentials', 'true')
-        # response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
         response.headers.add(
             'Access-Control-Allow-Methods',
             'GET,PUT,POST,DELETE,OPTIONS,PATCH')
